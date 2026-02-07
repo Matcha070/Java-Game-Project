076243b9a6b89276ca9a2e3063aaf84829a49bca
Matcha070 <162360656+Matcha070@users.noreply.github.com>
2026-02-07 22:40:06 +0700

    add damage

--- Bullet.java
+++ Bullet.java
@@ -7,11 +7,13 @@ public class Bullet implements DrawObj {
     double vx, vy;
     int size = 12;
     double speed;
+    int damage;
 
-    public Bullet(double x, double y, double speed) {
+    public Bullet(double x, double y, double speed, int damage) {
         this.x = x;
         this.y = y;
         this.speed = speed;
+        this.damage = damage;
     }
 
     public void update() {
@@ -21,8 +23,15 @@ public class Bullet implements DrawObj {
 
     @Override
     public void draw(Graphics g) {
-        System.out.println("Draw Bullet at (" + x + ", " + y + ")");
         g.setColor(Color.BLACK);
         g.fillOval((int)x - size/2, (int)y - size/2, size, size);
+    }
+
+    public boolean hitEnemy(Enemy e) {
+        double dx = e.getPosition().x - x;
+        double dy = e.getPosition().y - y;
+
+        double r = (e.getSize() / 2.0) + (size / 2.0);
+        return dx * dx + dy * dy <= r * r;
     }
 }
