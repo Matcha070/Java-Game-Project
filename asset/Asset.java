02f7d89208d1604b91fb81789a35b6f9b0460f9e
mxronee <143163167+mxronee@users.noreply.github.com>
2026-02-13 16:54:47 +0700

    SpriteSheet

--- asset/Asset.java
+++ asset/Asset.java
@@ -1,25 +1,63 @@
 package asset;
 
+import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class Asset {
-    //----------Map-----------
+    // ----------Map-----------
     public static BufferedImage GRASS;
     public static BufferedImage DIRT;
 
-    //---------Enemy----------
+    // ---------Enemy----------
     public static BufferedImage SLIME;
 
-    public static void load(){
+    public static void load() {
         try {
-            //----------Map-----------
+            // ----------Map-----------
             GRASS = ImageIO.read(Asset.class.getResource("/asset/map/Grass.png"));
             DIRT = ImageIO.read(Asset.class.getResource("/asset/map/Dirt.png"));
-            //---------Enemy----------
+            // ---------Enemy----------
             SLIME = ImageIO.read(Asset.class.getResource("/asset/enemy/Slime.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+}
+
+class SpriteSheet {
+    private BufferedImage[][] sprites;
+    private int rows, cols;
+
+    public SpriteSheet(String path, int spritesW, int spritesH) {
+        try {
+            BufferedImage sheet = ImageIO.read(new File(path));
+
+            cols = sheet.getWidth() / spritesW;
+            rows = sheet.getHeight() / spritesH;
+
+            sprites = new BufferedImage[cols][rows];
+
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    sprites[y][x] = sheet.getSubimage(
+                            x * spritesW,
+                            y * spritesH,
+                            spritesW,
+                            spritesH);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BufferedImage getFrame(int row, int frame) {
+        return sprites[row][frame];
+    }
+
+    public int getFrameCount() {
+        return cols;
+    }
+
 }
