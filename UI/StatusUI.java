3d42dd2fef412d5ccd5969d0d1237517e0d02c70
Matcha070 <162360656+Matcha070@users.noreply.github.com>
2026-02-15 23:58:51 +0700

    add padding

--- UI/StatusUI.java
+++ UI/StatusUI.java
@@ -1,9 +1,7 @@
 package UI;
 
 import GameController.GamePanel;
-
 import asset.Asset;
-
 import java.awt.*;
 import javax.swing.*;
 
@@ -22,13 +20,50 @@ public class StatusUI extends JPanel {
 
         super.paintComponent(g);
 
-        // Money--------
-        int currentMoney = game.getMoney().getAmount();
-        g.setFont(new Font("Tahoma", Font.BOLD, 22));
-        g.setColor(Color.WHITE);
-        g.drawString(String.valueOf(currentMoney), getWidth() - 140, getHeight() / 9 + 2);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g.drawImage(Asset.COIN_ICON, getWidth() - 200, getHeight() / 10 - 20, 48, 48, null);
+        int currentMoney = game.getMoney().getAmount();
+        String text = String.valueOf(currentMoney);
+
+        Font font = new Font("Tahoma", Font.BOLD, 22);
+        g2.setFont(font);
+
+        FontMetrics fm = g2.getFontMetrics();
+
+        int padding = 15;
+        int iconSize = 48;
+
+        // ตำแหน่ง icon
+        int iconX = getWidth() - 170;
+        int iconY = getHeight() / 10 - 20;
+
+        // ตำแหน่ง text
+        int textX = iconX + iconSize + 10;
+        int textY = iconY + iconSize - 15;
+
+        // คำนวณขนาดกรอบ
+        int boxX = iconX - padding;
+        int boxY = iconY - padding;
+        int boxWidth = iconSize + 10 + fm.stringWidth(text) + padding * 2;
+        int boxHeight = iconSize + padding * 2;
+
+        // วาดพื้นหลัง
+        g2.setColor(new Color(0, 0, 0, 150)); // ดำโปร่ง
+        g2.fillRoundRect(boxX, boxY, boxWidth, boxHeight, 20, 20);
+
+        // วาดขอบ
+        g2.setColor(Color.WHITE);
+        g2.setStroke(new BasicStroke(2f));
+        g2.drawRoundRect(boxX, boxY, boxWidth, boxHeight, 20, 20);
+
+        // วาดไอคอน
+        g2.drawImage(Asset.COIN_ICON, iconX, iconY, iconSize, iconSize, null);
+
+        // วาดข้อความ
+        g2.setColor(Color.WHITE);
+        g2.drawString(text, textX, textY);
 
     }
 }
